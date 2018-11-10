5d91f254690c579dbb9f6365b43192a301caecdc
yangxin <1393003255@qq.com>
2018-11-10 14:54:20 +0800

    ocProject_V402代码完成，测试成功！

--- ocPortal/src/main/java/com/online/college/portal/business/impl/PortalServiceImpl.java
+++ ocPortal/src/main/java/com/online/college/portal/business/impl/PortalServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.online.college.core.consts.CourseEnum;
 import com.online.college.core.consts.domain.ConstsClassify;
@@ -27,6 +28,7 @@ import com.online.college.portal.vo.CourseSectionVO;
  * 首页业务层
  * @author Admin!
  */
+@Service
 public class PortalServiceImpl implements IPortalService{
 
 	@Autowired
