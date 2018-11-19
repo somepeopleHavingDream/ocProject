694f3712cd37929624389a3bd1e5845c0943a0f8
yangxin <1393003255@qq.com>
2018-11-19 22:17:56 +0800

    完成ocProject_v501_04的代码工作，实战推荐、免费好课、java课程。

--- ocPortal/src/main/java/com/online/college/portal/controller/PortalController.java
+++ ocPortal/src/main/java/com/online/college/portal/controller/PortalController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.online.college.core.consts.CourseEnum;
 import com.online.college.core.consts.domain.ConstsSiteCarousel;
 import com.online.college.core.consts.service.IConstsSiteCarouselService;
+import com.online.college.core.course.domain.Course;
+import com.online.college.core.course.domain.CourseQueryDto;
+import com.online.college.core.course.service.ICourseService;
 import com.online.college.portal.business.IPortalBusiness;
 import com.online.college.portal.vo.ConstsClassifyVO;
 
@@ -24,6 +28,9 @@ public class PortalController {
 	
 	@Autowired
 	private IConstsSiteCarouselService siteCarouselService;
+	
+	@Autowired
+	private ICourseService courseService;
 	
 	/**
 	 * 首页
@@ -42,6 +49,27 @@ public class PortalController {
 		// 课程推荐
 		portalBusiness.prepareRecomdCourse(classifys);
 		mv.addObject("classifys", classifys);
+		
+		// 获取5门实战课推荐，根据权重(weight）进行排序
+		CourseQueryDto queryEntity = new CourseQueryDto();
+		queryEntity.setCount(5);	// 5门
+		queryEntity.setFree(CourseEnum.FREE_NOT.value());	// 非免费的：实战课
+		queryEntity.descSortField("weight");// 按照weight降序排列
+		List<Course> actionCourseList = this.courseService.queryList(queryEntity);
+		mv.addObject("actionCourseList", actionCourseList);
+		
+		// 获取5门免费课推荐，根据权重（weight）进行排序
+		queryEntity.setFree(CourseEnum.FREE.value());// 免费
+		List<Course> freeCourseList = this.courseService.queryList(queryEntity);
+		mv.addObject("freeCourseList", freeCourseList);
+		
+		// 获取7门java课程，根据权重（学习数量studyCount）进行排序
+		queryEntity.setCount(7);
+		queryEntity.setFree(null);// 不分实战和免费类别
+		queryEntity.setSubClassify("java");// java分类
+		queryEntity.descSortField("studyCount");// 按照studyCount降序排列
+		List<Course> javaCourseList = this.courseService.queryList(queryEntity);
+		mv.addObject("javaCourseList", javaCourseList);
 		
 		return mv;
 	}
