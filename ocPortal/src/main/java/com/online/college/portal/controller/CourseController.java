8ed0cf7023f353d60b0d8b76f9d4b811899b419e
yangxin <1393003255@qq.com>
2018-12-07 17:26:33 +0800

    完成ocProject_v503_02版本的控制层部分，包含了课程信息、课程章节、教师、推荐课程的后台代码。

--- ocPortal/src/main/java/com/online/college/portal/controller/CourseController.java
+++ ocPortal/src/main/java/com/online/college/portal/controller/CourseController.java
@@ -1,129 +1,72 @@
 package com.online.college.portal.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import javax.annotation.Resource;
-
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.online.college.common.page.TailPage;
-import com.online.college.common.web.JsonView;
-import com.online.college.core.consts.domain.ConstsClassify;
+import com.online.college.common.storage.QiniuStorage;
+import com.online.college.core.auth.domain.AuthUser;
+import com.online.college.core.auth.service.IAuthUserService;
 import com.online.college.core.course.domain.Course;
+import com.online.college.core.course.domain.CourseQueryDto;
 import com.online.college.core.course.service.ICourseService;
-import com.online.college.portal.business.IPortalService;
-import com.online.college.portal.vo.ConstsClassifyVO;
+import com.online.college.portal.business.ICourseBusiness;
 import com.online.college.portal.vo.CourseSectionVO;
 
 /**
- * 课程管理
+ * 课程详情页
  */
 @Controller
-//@RequestMapping("/course")
+@RequestMapping("/course")
 public class CourseController {
 	
-	@Resource
+	@Autowired
+	private ICourseBusiness courseBusiness;
+	
+	@Autowired
 	private ICourseService courseService;
 	
 	@Autowired
-	private IPortalService portalService;
+	private IAuthUserService authUserService;
 	
-	/**
-	 * 课程管理
-	 */
-	@RequestMapping("/pagelist")
-	public ModelAndView list(Course queryEntity, TailPage<Course> page) {
-		ModelAndView mv = new ModelAndView("cms/course/pagelist");
+	@RequestMapping("/learn/{courseId}")
+	public ModelAndView learn(@PathVariable Long courseId) {
+		if (null == courseId) {
+			return new ModelAndView("error/404");
+		}
 		
-		page.setPageSize(5);
-		page = courseService.queryPage(queryEntity, page);
-		mv.addObject("page", page);
-		
-		mv.addObject("curNav", "course");
-		return mv;
-	}
-	
-	/**
-	 * 课程编辑
-	 */
-	@RequestMapping("/read")
-	public ModelAndView courseRead(Long id) {
-		Course course = courseService.getById(id);
+		// 获取课程
+		Course course = courseService.getById(courseId);
 		if (null == course) {
 			return new ModelAndView("error/404");
 		}
 		
-		ModelAndView mv = new ModelAndView("cms/course/read");
-		mv.addObject("curNav", "course");
+		// 获取课程章节
+		ModelAndView mv = new ModelAndView("learn");
+		List<CourseSectionVO> chaptSections = this.courseBusiness.queryCourseSection(courseId);
 		mv.addObject("course", course);
-		
-		List<CourseSectionVO> chaptSections = this.portalService.queryCourseSection(1L);
 		mv.addObject("chaptSections", chaptSections);
 		
-		Map<String, ConstsClassifyVO> classifyMap = portalService.queryAllClassifyMap();
-		
-		// 所有一级分类
-		List<ConstsClassifyVO> classifysList = new ArrayList<ConstsClassifyVO>();
-		for (ConstsClassifyVO vo : classifyMap.values()) {
-			classifysList.add(vo);
+		// 获取讲师
+		AuthUser courseTeacher = this.authUserService.getByUsername(course.getUsername());
+		if (StringUtils.isNotEmpty(courseTeacher.getHeader())) {
+			courseTeacher.setHeader(QiniuStorage.getUrl(courseTeacher.getHeader()));
 		}
-		mv.addObject("classifys", classifysList);
+		mv.addObject("courseTeacher", courseTeacher);
 		
-		List<ConstsClassify> subClassifys = new ArrayList<ConstsClassify>();
-		for (ConstsClassifyVO vo : classifyMap.values()) {
-			subClassifys.addAll(vo.getSubClassifyList());
-		}
-		mv.addObject("subClassifys", subClassifys);	// 所有二级分类
+		// 获取推荐课程
+		CourseQueryDto queryEntity = new CourseQueryDto();
+		queryEntity.descSortField("weight");
+		queryEntity.setCount(5);// 5门推荐课程
+		queryEntity.setSubClassify(course.getSubClassify());
+		List<Course> recomdCourseList = this.courseService.queryList(queryEntity);
+		mv.addObject("recomdCourseList", recomdCourseList);
 		
-		return mv;
-	}
-	
-	/**
-	 * 课程上下架
-	 */
-	@RequestMapping("/doSale")
-	@ResponseBody
-	public String doSale(Course entity) {
-		courseService.updateSelectivity(entity);	// 更新课程总时长
-		
-		return new JsonView().toString();
-	}
-	
-	/**
-	 * 课程上下架
-	 * @param entity
-	 * @return
-	 */
-	@RequestMapping("/doDelete")
-	@ResponseBody
-	public String doDelete(Course entity) {
-		courseService.delete(entity);
-		
-		return new JsonView().toString();
-	}
-	
-	/**
-	 * 根据id获取
-	 */
-	@RequestMapping("/getById")
-	@ResponseBody
-	public String getById(Long id) {
-		return JsonView.render(courseService.getById(id));
-	}
-	
-	/**
-	 * 添加课程
-	 */
-	@RequestMapping("/add")
-	public ModelAndView add() {
-		ModelAndView mv = new ModelAndView("cms/course/add");
-		mv.addObject("curNav", "course");
 		return mv;
 	}
 }
--- ocService/src/main/java/com/online/college/core/course/domain/CourseSection.java
+++ ocService/src/main/java/com/online/college/core/course/domain/CourseSection.java
@@ -5,92 +5,98 @@ import com.online.college.common.orm.BaseEntity;
 /**
  * 课程章节
  */
-public class CourseSection extends BaseEntity{
+public class CourseSection extends BaseEntity {
 
 	private static final long serialVersionUID = -7261405404725335316L;
 
 	/**
-	*归属课程id
-	**/
+	 * 归属课程id
+	 **/
 	private Long courseId;
 
 	/**
-	*父章节id，（只有2级）
-	**/
+	 * 父章节id，（只有2级）
+	 **/
 	private Long parentId;
 
 	/**
-	*章节名称
-	**/
+	 * 章节名称
+	 **/
 	private String name;
 
 	/**
-	*排序
-	**/
+	 * 排序
+	 **/
 	private Integer sort;
 
 	/**
-	*时长
-	**/
+	 * 时长
+	 **/
 	private String time;
 
 	/**
-	*未上架（0）、上架（1）
-	**/
+	 * 未上架（0）、上架（1）
+	 **/
 	private Integer onsale;
-	
+
 	/**
 	 * 视频url
 	 */
 	private String videoUrl;
 
-	public Long getCourseId(){
+	public Long getCourseId() {
 		return courseId;
 	}
-	public void setCourseId(Long courseId){
+
+	public void setCourseId(Long courseId) {
 		this.courseId = courseId;
 	}
 
-	public Long getParentId(){
+	public Long getParentId() {
 		return parentId;
 	}
-	public void setParentId(Long parentId){
+
+	public void setParentId(Long parentId) {
 		this.parentId = parentId;
 	}
 
-	public String getName(){
+	public String getName() {
 		return name;
 	}
-	public void setName(String name){
+
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public Integer getSort(){
+	public Integer getSort() {
 		return sort;
 	}
-	public void setSort(Integer sort){
+
+	public void setSort(Integer sort) {
 		this.sort = sort;
 	}
 
-	public String getTime(){
+	public String getTime() {
 		return time;
 	}
-	public void setTime(String time){
+
+	public void setTime(String time) {
 		this.time = time;
 	}
 
-	public Integer getOnsale(){
+	public Integer getOnsale() {
 		return onsale;
 	}
-	public void setOnsale(Integer onsale){
+
+	public void setOnsale(Integer onsale) {
 		this.onsale = onsale;
 	}
+
 	public String getVideoUrl() {
 		return videoUrl;
 	}
+
 	public void setVideoUrl(String videoUrl) {
 		this.videoUrl = videoUrl;
 	}
-	
 }
-
